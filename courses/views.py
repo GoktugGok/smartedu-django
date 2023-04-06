2618d3a0cc5d6af1ee8ef1fc528f75aa533cef34
GoktugGok <gokgoktug0@gmail.com>
2023-04-06 07:48:38 +0300

    courses

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from . models import Courses
 
-# Create your views here.
+def course_list(request):
+    courses = Courses.objects.all().order_by('-date')
+    context = {
+        'courses': courses
+    }
+    return render(request, 'courses.html', context)
