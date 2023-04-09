9c050843ff76b9b00b44ab0c46c9c1f2f895ce8c
GoktugGok <gokgoktug0@gmail.com>
2023-04-09 06:13:07 +0300

    course-add

--- courses/admin.py
+++ courses/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from . models import Courses
+from . models import Courses ,Category
 
 @admin.register(Courses)
 class CourseAdmin(admin.ModelAdmin):
     list_display = ('name','available')
     list_filter = ('available',)
     search_fields = ('name', 'description')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields ={'slug':('name',)}
 
 # Register your models here.
--- courses/views.py
+++ courses/views.py
@@ -7,3 +7,12 @@ def course_list(request):
         'courses': courses
     }
     return render(request, 'courses.html', context)
+
+def course_detail(request, category_slug, course_id):
+    course = Courses.objects.get(category__slug=category_slug,id= course_id)
+
+    context = {
+        'course': course
+    }
+
+    return render(request,'course.html',context)
