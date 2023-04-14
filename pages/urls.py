f7bdeda4132e1a17d2bfec9f8319c2d9197cb87a
GoktugGok <gokgoktug0@gmail.com>
2023-04-15 00:01:54 +0300

    teachers

--- pages/urls.py
+++ pages/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from pages.views import IndexView,AboutView
 
 urlpatterns = [
-    path('',IndexView.as_view(),name="home"),
+    path('',IndexView.as_view(), name="home"),
     path('about/',AboutView.as_view(),name="about")
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,37 +2,22 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from courses.models import Courses
 
-
-# from django.views.generic import TemplateView
-# from courses.models import Courses
-
 class IndexView(TemplateView):
     template_name = 'index.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['courses'] = Courses.objects.filter(available=True).order_by("-date")[:2]
+        context['total_course'] = Courses.objects.filter(available=True).count()
         return context
-
+        
 class AboutView(TemplateView):
     template_name = 'about.html'
 
-# class IndexView(TemplateView):
-#     template_name = 'index.html'
-
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context['courses'] = Courses.objects.filter(available=True).order_by("-date")[:2]
-#         context['total_course'] = Courses.objects.filter(available=True).count()
-#         return context
 
 # def home(request):
 #      return render(request, 'index.html')
 
-# class AboutView(TemplateView):
-#     template_name = 'about.html'
-
 # def about(request):
 #     return render(request, 'about.html')
 
-# Create your views here.
