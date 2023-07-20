b70c050cb389a282820f12aaaa66afc592ac427c
GoktugGok <gokgoktug0@gmail.com>
2023-07-21 01:42:58 +0300

    cancel_the_course

--- accounts/views.py
+++ accounts/views.py
@@ -68,4 +68,11 @@ def enroll_the_course(request):
     course.students.add(user)
     return redirect('dashboard')
 
-
+def cancel_the_course(request):
+    course_id = request.POST['course_id']
+    user_id = request.POST['user_id']
+    course_cancel = Courses.objects.get(id = course_id)
+    user = User.objects.get(id = user_id)
+    course_cancel.students.remove(user)
+    return redirect('dashboard')
+    
